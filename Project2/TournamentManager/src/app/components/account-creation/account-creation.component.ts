d87cfe33e4d5e32179e65f2ccf20fdc0b59519c7
Joseph Milne <acornmanjam@yahoo.com>
2019-02-10 21:46:22 -0500

    Auto stash before merge of "Joe" and "origin/Joe"

--- Project2/TournamentManager/src/app/components/account-creation/account-creation.component.ts
+++ Project2/TournamentManager/src/app/components/account-creation/account-creation.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-creation.component.css']
 })
 export class AccountCreationComponent implements OnInit {
-
+  username: string;
+  password: string;
+  email: string;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+    console.log("Submit button is displaying this.");
+    console.log("Username of organizer is: ", this.username);
+    console.log("Password of organizer is: ", this.password);
+    console.log("E-mail of organizer is: ", this.email);
+  }
 }
