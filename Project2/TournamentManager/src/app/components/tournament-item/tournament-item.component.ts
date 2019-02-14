59c376757b4f917083ad9b6c572a3c3260d65b0b
csGrebo <oberg.chris@yahoo.com>
2019-02-14 10:36:25 -0500

    work towards tournament item

--- Project2/TournamentManager/src/app/components/tournament-item/tournament-item.component.ts
+++ Project2/TournamentManager/src/app/components/tournament-item/tournament-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Match} from "../../model/match";
 import {Round} from "../../model/round";
+import {MatchmakingService} from "../../services/matchmaking/matchmaking.service";
 
 @Component({
   selector: 'app-tournament-item',
@@ -12,7 +13,7 @@ export class TournamentItemComponent implements OnInit {
   currentRound: Round;
   roundMatches: Array<Match>;
   constructor(
-
+    private matchmakingService: MatchmakingService
   ) { }
 
   ngOnInit() {
