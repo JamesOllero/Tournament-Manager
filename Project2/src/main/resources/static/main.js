eb27369c47bbea92da3ef31ebbe8ab90d9e66ef1
csGrebo <oberg.chris@yahoo.com>
2019-02-18 09:16:55 -0500

    Presented Version

--- Project2/src/main/resources/static/main.js
+++ Project2/src/main/resources/static/main.js
@@ -2010,7 +2010,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Star\Desktop\project_2\project2\TournamentManager\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! D:\Programming\project_2\Project2\TournamentManager\src\main.ts */"./src/main.ts");
 
 
 /***/ })
